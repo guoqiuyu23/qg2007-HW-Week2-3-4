4010af7115973ee86cdcf8ae9dfc68074e8fe84a
LAPTOP-2HQ0J53J\qiuyu <guoqiuyu23@gmail.com>
2021-03-01 17:43:43 -0500

    update

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float forceAmount = 5;
+    private Rigidbody2D rb2d;
+
+    public static PlayerControl instance;
+    
+    public GameObject projectilePrefab;
+    public float horizontalInput;
+    public float speed= 5 ;
+    
+    
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForce(Vector2.right*forceAmount);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForce(Vector2.left*forceAmount);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Script/Prize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+
+namespace Script
+{
+    public class Prize : MonoBehaviour
+    {
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Destroy(gameObject);
+            GameManager.instance.score++;
+
+        }
+    }
+}
