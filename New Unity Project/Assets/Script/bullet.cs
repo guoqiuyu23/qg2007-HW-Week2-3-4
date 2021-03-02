e76d438e306513660e3793a178956c9cf60ad14e
LAPTOP-2HQ0J53J\qiuyu <guoqiuyu23@gmail.com>
2021-03-01 21:30:18 -0500

    New Unity Proj

---
+++ New Unity Project/Assets/Script/bullet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    private Rigidbody2D rb2d2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
